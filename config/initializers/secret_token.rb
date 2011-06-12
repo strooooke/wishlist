5e2df99db707dee8ae55a3e601e743fe219e3f7c
stroke <stroke@strokeee.(none)>
2011-06-12 15:25:50 +0200

    new project with homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishlist::Application.config.secret_token = '1ad6c8cb1dfd6dc24b9dacd1dea8231b2d98355508d4ca434693b0d7ed141e98dd49fcf2f7a158e5c38193be8816472e100b346d4aaf7857be867f9f137326ad'
